a45b6f3e5ba944ce0460ce9ca411ef91febe71bb
Kateryna Verveda <katushka211@gmail.com>
2023-04-20 11:34:24 +0200

    creat GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+const theme = {
+  colors: {
+    lightgrey: '#dde5de',
+    grey: '#747774',
+    white: '#fff',
+    black: '#212122',
+    textgrey: '#9b9ba0',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+    <GlobalStyle />
   </React.StrictMode>
 );
